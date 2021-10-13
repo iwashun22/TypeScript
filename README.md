793407d5e4db626a4a2aed9534263549e03dad3a
Shun Iwashita <iwashunabc519@gmail.com>
2021-10-13 14:54:21 +0700

    add description

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 
 This is my first time of learning TypeScript
 
+## What is TypeScript
+
+TypeScript is a strongly typed programming language that compile as a JavaScript file
+
+## How it works
+
+### Note:
+
+> TypeScript does not run on the browser
+
+```tsc <filename>```
+
+You need to run this command to compile as a JavaScript file, to get executed on the browser
--- app.ts
+++ app.ts
@@ -1,1 +1,15 @@
-console.log('preparing...');
+// run tsc <filename> and it will compile to JavaScript file
+// note: TypeScript does not run on the browser
+
+function add(num1: number, num2: number){
+   return num1 + num2;
+}
+
+const n1 = '5';
+const n2 = 3.9;
+
+// const wrong = add(n1, n2); // error on the terminal
+const correct = add(+n1, n2); // fine
+
+// console.log(wrong);
+console.log(correct);
