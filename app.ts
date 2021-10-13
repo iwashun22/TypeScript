43bd7fc2029db04e9bf74d83e06ed2a551c40988
Shun Iwashita <iwashunabc519@gmail.com>
2021-10-13 15:55:44 +0700

    update
    first lesson

--- app.ts
+++ app.ts
@@ -1,7 +1,21 @@
+// const person: {
+//    name: string;
+//    age: number;
+// } = 
 const person = {
    name: 'Jackie',
-   age: 30
+   age: 30,
+   hobbies: ['Sports', 'Programming']
 }
 
-person.nickname = 'Jack';
-console.log(person.nickname);
+let languages: string[];
+// languages = ['C++', 2]; // error
+languages = ['C++', 'JavaScript'];
+
+console.log(person.name);
+
+for(const hobby of person.hobbies){
+   console.log(hobby.toUpperCase()); // this worked because its type of string
+   //console.log(hobby.map()); 
+   // this doesn't work becaue it's not for a string
+}
