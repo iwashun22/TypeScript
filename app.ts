331657ff8788ff0d63d44be1e9c63d2f835550eb
Shun Iwashita <iwashunabc519@gmail.com>
2021-10-13 15:15:28 +0700

    update

--- app.ts
+++ app.ts
@@ -1,13 +1,12 @@
 
 function add(num1: number, num2: number){
-   console.log(typeof num1);
    return num1 + num2;
 }
 
 const n1 = '5';
 const n2 = 3.9;
 
-// const wrong = add(n1, n2); // error on the terminal
+// const wrong = add(n1, n2); // throw error on the terminal
 const correct = add(+n1, n2); // fine
 
 // console.log(wrong);
