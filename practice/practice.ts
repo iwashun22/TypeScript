723c3f0af7d54d40e03b318b0f0be881f6710d93
Shun Iwashita <iwashunabc519@gmail.com>
2021-10-15 18:56:27 +0700

    practice one

--- practice/practice.ts
+++ practice/practice.ts
@@ -1,12 +1,17 @@
 
-function combine(input1: number | string, input2: number | string){
-   if(typeof input1 == 'number' && typeof input2 == 'number'){
-      return input1 + input2;
+function combine(
+   input1: number | string, 
+   input2: number | string,
+   arg: 'as-number' | 'as-text'
+){
+   if(arg == 'as-number'){
+      return Number(input1) + Number(input2);
    }
    else {
       return input1.toString() + input2.toString();
    }
 }
 
-console.log(combine('hello ', 'world'));
-console.log(combine(13, 64));
+console.log(combine('hello ', 'world', 'as-text'));
+console.log(combine(13, 64, 'as-text'));
+console.log(combine('15', '28', 'as-number'));
