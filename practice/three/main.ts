a499add1e058f2b4da3e0a050c27c2e80bc011d9
Shun Iwashita <iwashunabc519@gmail.com>
2021-10-16 10:43:04 +0700

    update

--- practice/three/main.ts
+++ practice/three/main.ts
@@ -25,7 +25,22 @@ const person_two: Person = {
    isMale: 'no'
 }
 
-addAndCallback(person_one.age, person_two.age, (result) => {
+addAndCallback(person_one.age, person_two.age, result => {
    const mean = result / 2;
    console.log(`The average age is: ${mean}`);
+})
+
+type Triangle = {
+   base: number,
+   height: number
+}
+
+const triangle: Triangle = {
+   base: 15,
+   height: 30
+}
+
+multiplyAndCallback(triangle.base, triangle.height, result => {
+   const area = result / 2;
+   console.log(`The area of triangle is ${area}`);
 })
