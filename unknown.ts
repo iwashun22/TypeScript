cc049e16063345a09c3120266ea41bfdec67750d
Shun Iwashita <iwashunabc519@gmail.com>
2021-10-16 14:58:34 +0700

    add unknown.ts
    explain about what is unknown

---
+++ unknown.ts
@@ -0,0 +1,15 @@
+// Unknown is a variable that is similar to any but more strict
+// here is the example
+
+let unknown: unknown; // can hold any type of variable 
+let any: any; // can hold any type of variable
+let string: string;
+
+any = 5;
+any = 'Hello world'
+
+unknown = 6;
+unknown = 'Goodbye world';
+
+string = any;
+string = unknown; // error
